105bbd82a9a3f32bd8d9c5366e3abd6dee62ba98
Kriszha Real <yujmin901205@gmail.com>
2024-10-04 06:00:55 +0800

    Add project files.

---
+++ StructAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 187.29m;
+
+            Console.WriteLine("Amount: $" + number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
